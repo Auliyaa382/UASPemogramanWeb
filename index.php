7a7ad108fb978cc1372403632066acffa53b7984
Auliya <auliyaa382@gmail.com>
2022-01-09 19:06:23 +0700

    keranjang transaksi

--- index.php
+++ index.php
@@ -7,27 +7,39 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Detail Transaksi</h2>
+    <h2>Keranjang Transaksi</h2>
 <table border="1">
     <tr>
+        <td>No.</td>
         <td>NAMA PRODUK</td>
         <td>HARGA</td>
         <td>QTY</td>
         <td>SUBTOTAL</td>
+        <td>AKSI</td>
 
     </tr>
         <?php
+        $n =1;
         include "koneksi.php";
         $query = mysqli_query($koneksi, 'SELECT * FROM tbproduk JOIN tbdetail on tbproduk.id_produk=tbdetail.id_produk');
         while ($data = mysqli_fetch_array($query)) {
         ?>
+        
             <tr>
+                <td><?php echo $n++;?></td>
                 <td><?php echo $data['nama_produk'] ?></td>
                 <td><?php echo $data['harga'] ?></td>
                 <td><?php echo 'x'; echo $data['qty'] ?></td>
                 <td><?php echo $data['harga']*$data['qty'] ?></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $data['id_produk'];?>">EDIT</a>
+                    <a href="hapus.php?id=<?php echo $data['id_produk'];?>">DELETE</a>
+                </td>
             </tr>
         <?php } ?>
  </table>
+            <br>
+            <a href="menu.php">Lanjut Belanja</a>
+            <a href="checkout.php">Checkout</a>
 </body>
 </html>
