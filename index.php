4e10976422b1ae168a7bf2ff1da47d3df52fb32e
Auliya <auliyaa382@gmail.com>
2022-01-13 21:09:23 +0700

    9.09 PM

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <h2>Keranjang Transaksi</h2>
-<table border="1">
+<table>
     <tr>
         <td>No.</td>
         <td>NAMA PRODUK</td>
@@ -16,7 +17,6 @@
         <td>QTY</td>
         <td>SUBTOTAL</td>
         <td>AKSI</td>
-
     </tr>
         <?php
         $n =1;
@@ -26,16 +26,31 @@
         ?>
         
             <tr>
+                <?php $data['subtotal'] = $data['harga']*$data['qty'] ?>
                 <td><?php echo $n++;?></td>
                 <td><?php echo $data['nama_produk'] ?></td>
                 <td><?php echo $data['harga'] ?></td>
-                <td><?php echo 'x'; echo $data['qty'] ?></td>
-                <td><?php echo $data['harga']*$data['qty'] ?></td>
+                <td><?php echo $data['qty'] ?></td>
+                <td><?php echo $data['subtotal'] ?></td>
                 <td>
                     <a href="hapus.php?id=<?php echo $data['id_produk'];?>">Hapus</a>
                 </td>
+                <?php @$total += $data['subtotal']?>
             </tr>
+            
+            <!-- <tr>
+                <?php $data['total'] = $data['subtotal']?>
+                <td>TOTAL</td>
+                <td><?php $data['total']?></td>
+            </tr> -->
         <?php } ?>
+        <tr>
+            <td>TOTAL</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><?php echo $total;?></td>
+        </tr>
  </table>
             <br>
             <a href="menu.php">Lanjut Belanja</a>
